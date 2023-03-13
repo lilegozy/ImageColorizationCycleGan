77e8287ef3e3f3e4add0cff19c7f993a6a7af395
lilegozy <natiedits@gmail.com>
2023-03-13 08:56:32 +0200

    second commit update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
+from  PIL import Image, ImageEnhance
 
-st.header('st.button')
+st.header('Image Colorization using cycle gan :)')
 
-if st.button('Say hello'):
-     st.write('Why hello there')
-else:
-     st.write('Goodbye')
+uploaded_file = st.file_uploader("Choose a file",type=['jpg','png','jpeg'])
+if uploaded_file is not None:
+    # To read file as bytes:
+    image = Image.open(uploaded_file)
+    col1, col2 = st.columns( [0.5, 0.5])
+    with col1:
+        st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+        st.image(image,width=300)  
+
+    with col2:
+        st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+
